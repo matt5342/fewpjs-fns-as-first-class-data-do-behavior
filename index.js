9c7c512285946fcd37e1a17d36672b4d5a5e484f
Matt <mattfsewell@gmail.com>
2021-02-23 16:02:20 -0500

    commit tree 6afc5977f3425ff71d9c3adc01fc101c390069f3

--- index.js
+++ index.js
@@ -11,3 +11,18 @@ function handleClick(e) {
 
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
+function greet(time) {
+  let hour = time.split(":")[0]
+  if (hour < 12){
+    return "Good Morning"
+  }
+  else if (hour > 12 && hour < 17){
+    return "Good Afternoon"
+  }
+  else{
+    return "Good Evening"
+  }
+}
+function displayMessage(message) {
+  document.getElementById("greeting").innerText = message
+}
